ff4dd241da269694813c6efd04a43c1231279c6e
martham0 <martha060997@gmail.com>
2023-09-12 15:12:47 -0700

    basic api endpoints

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-const http = require('http');
- 
+const express = require('express');
+const app = express ();
+app.use(express.json());
 const hostname = 'localhost';
-const port = 3000;
- 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
- 
-server.listen(port, hostname, () => {
+const port = 3000
+
+// info
+
+class crew {
+  constructor(name, age, position, luffyRank) {
+    this.name = name;
+    this.age = age;
+    this.position = position;
+    this.luffyRank = luffyRank;
+  }
+}
+
+const sanji = new crew('Vinsmoke Sanji', 23, 'Cook','4');
+const nami = new crew('Nami', 22, 'Navigator','2');
+const zoro = new crew('Roronoa Zoro', 23, '2nd in command','1');
+const luffy = new crew('Monkey D. Luffy', 21, 'Captain','0');
+const usopp = new crew('Usopp', 19, 'Sniper','3');
+
+const strawhat_crew = [sanji,nami,zoro,luffy,usopp]
+
+app.listen(port,hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+app.get("/status", (req,res) => {
+  const status = {
+    "Status": "Running"
+ };
+ res.send(status)
+});
+
+app.get("/strawhat-crew", (req,res) => {
+  res.json(strawhat_crew)
+  
+})
+
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+ 
