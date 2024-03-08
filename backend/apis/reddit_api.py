32f66bbe46e05d6e34b95feaada82d8f8278cc22
martham0 <martha060997@gmail.com>
2024-03-07 19:02:12 -0800

    formatting

--- backend/apis/reddit_api.py
+++ backend/apis/reddit_api.py
@@ -6,16 +6,34 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 # Import config values
 config = configparser.ConfigParser()
 config.read("../config.ini")
+print(f"\n{config['Reddit_creds']['client_id']}")
 
+# Initialize sentiment analysis Instance 
+analyzer = SentimentIntensityAnalyzer()
 
+# Create Reddit instance
 reddit = praw.Reddit(
-    # username=f"${config['Reddit_creds']['username']}",
-    # password=f"${config['Reddit_creds']['password']}",
     client_id=f"{config['Reddit_creds']['client_id']}",
     client_secret=f"{config['Reddit_creds']['client_secret']}",
     user_agent=f"{config['Reddit_creds']['user_agent']}",
 )
-analyzer = SentimentIntensityAnalyzer()
+
+
+def character_scores(character_name):
+  subreddit = "OnePiece"
+  content = []
+  score = []
+  # search all posts of specified character and append the content of post to an array
+  for submission in reddit.subreddit(subreddit).search(query=character_name, sort="hot",time_filter="day",syntax="plain"):
+      print(f"**{submission.title}**\n")
+      # contents of the post
+      content.append(submission.selftext)
+  # calculate sentiment score for every post
+  for post in content:
+      vs = analyzer.polarity_scores(post)
+      score.append(vs["compound"])
+  return score
+  
 def character_sentiment(sentiment_scores):
   average_score = statistics.mean(sentiment_scores)
   print(f'average score:{average_score}')
@@ -25,26 +43,13 @@ def character_sentiment(sentiment_scores):
     return "negative"
   else:
     return "neutral"
-
       
 def main():
-  subreddit = "OnePiece"
   character = "luffy"
-  content = []
-  score = []
 #  search subreddit for a specific character submission on the current day
-  for submission in reddit.subreddit(subreddit).search(query=character, sort="hot",time_filter="day",syntax="plain"):
-        print(f"**{submission.title}**\n")
-        # contents of the post
-        content.append(submission.selftext)
-        
-
-  for post in content:
-      vs = analyzer.polarity_scores(post)
-      score.append(vs["compound"])
-  print(f"Number of posts:{len(score)}")
-  print(f"THIS ARE THE OG SCORES{(score)}")
-  print(f"{character} Sentiment: {character_sentiment(score)}")
+  scores = character_scores(character)
+  print(f"THIS ARE ALL THE POST SCORES{scores}")
+  print(f"{character} Sentiment: {character_sentiment(scores)}")
       
 
   
